c97ec65cc1bba4cc1ed6f5b073d9ae25dae512b2
notangelmario <hello@notangelmario.dev>
2024-12-07 22:53:34 +0200

    Add license page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,8 +13,8 @@ export function Footer(props: FooterProps) {
 
 	return (
 		<footer className="bg-background border border-t-muted w-full p-16">
-			<div className="container">
-				<div className="flex flex-row gap-2">
+			<div className="container flex flex-row gap-16">
+				<div className="flex flex-row gap-4">
 					<Logo
 						width={32}
 						height={32}
@@ -23,7 +23,17 @@ export function Footer(props: FooterProps) {
 						<p>
 							Dovedit
 						</p>
+						<a href="/licenta">
+							FSL, Version 1.1, ALv2 Future License
+						</a>
 					</div>
+				</div>
+
+				<div className="flex flex-col">
+					<h2 className="text-muted-foreground">SOCIAL</h2>
+					<a href="https://opencollective.com/dovedit">OpenCollective</a>
+					<a href="https://github.com/dovedit">GitHub</a>
+					<a href="https://instagram.com/doveditro">Instagram</a>
 				</div>
 			</div>
 		</footer>
